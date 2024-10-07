62ca732ceb714e472c073422a487f0f307c70ca8
Michael Reuter <mareuternh@gmail.com>
2024-10-06 19:55:52 -0700

    Corrected import.

--- src/aio_stats/data_reader.py
+++ src/aio_stats/data_reader.py
@@ -4,7 +4,7 @@
 
 import pathlib
 
-import pyarrow as pa
+import pyarrow.dataset as ds
 import pyarrow.parquet as pq
 
 __all__ = ["DataReader"]
@@ -20,5 +20,18 @@ class DataReader:
         data_dir : pathlib.Path
             Directory containing the structure data.
         """
-        p = pa.dataset.partitioning(field_names=["year", "month"])
-        self.table = pq.read_table(data_dir, partitioning=p)
+        self.data_dir = data_dir.expanduser()
+
+    def read_all(self) -> None:
+        """Read all data from directory."""
+        p = ds.partitioning(field_names=["year", "month"])
+        self.table = pq.read_table(self.data_dir, partitioning=p)
+
+    def read_month(self) -> None:
+        """Read data from specific month."""
+        self.table = pq.read_table(self.data_dir)
+
+    def read_year(self) -> None:
+        """Read data from specific year."""
+        p = ds.partitioning(field_names=["month"])
+        self.table = pq.read_table(self.data_dir, partitioning=p)
