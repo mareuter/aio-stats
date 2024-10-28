69605a54c3e25c67d276e4e6f735893f50cd9493
Michael Reuter <mareuternh@gmail.com>
2024-10-27 19:59:18 -0700

    Switch to internal config for collect_stats.

--- src/aio_stats/collect_stats.py
+++ src/aio_stats/collect_stats.py
@@ -4,6 +4,7 @@
 
 import argparse
 from datetime import datetime, timedelta
+from importlib.resources import files
 import pathlib
 import tomllib
 from zoneinfo import ZoneInfo
@@ -15,22 +16,26 @@ def main(opts: argparse.Namespace) -> None:
     now = datetime.now(ZoneInfo(opts.timezone))
     yesterday = now - timedelta(days=1)
 
-    config_file: pathlib.Path = opts.config_file.expanduser()
-    with config_file.open("rb") as cfile:
-        cdict = tomllib.load(cfile)
+    stat_feeds_file = files("aio_stats.data").joinpath("stat_feeds.toml")
+    stat_feeds = tomllib.loads(stat_feeds_file.read_text())
+
+    if opts.location is not None:
+        locations = [opts.location]
+    else:
+        locations = list(stat_feeds["locations"])
 
     aioclient = aio_stats.AioClient()
 
-    for entry in cdict:
-        for feed in cdict[entry]["feeds"]:
-            print(f"Processing {entry}.{feed}")
-            data = aioclient.fetch_data(f"{entry}.{feed}", max_points=350)
+    for location in locations:
+        for feed in stat_feeds["locations"][location]["feeds"]:
+            print(f"Processing {location}.{feed}")
+            data = aioclient.fetch_data(f"{location}.{feed}", max_points=350)
             tdata = aioclient.transform_data(data, opts.timezone)
             stats = aio_stats.StatsMaker()
             stats.create_dataframe(tdata, feed)
             stats.filter_time(yesterday, now, opts.day_bound)
             stats.make_stats()
-            stats.save_stats(opts.output_dir, entry)
+            stats.save_stats(opts.output_dir, location)
 
 
 def runner() -> None:
@@ -39,15 +44,15 @@ def runner() -> None:
     parser.add_argument(
         "output_dir", type=pathlib.Path, help="Location for stats output."
     )
-    parser.add_argument(
-        "config_file",
-        type=pathlib.Path,
-        help="Configuration of feeds to collect stats for.",
-    )
+
     parser.add_argument("--timezone", type=str, help="Set the timezone.")
 
     parser.add_argument(
         "--day-bound", action="store_true", help="Truncate timestamps to day bounds."
+    )
+
+    parser.add_argument(
+        "--location", help="Provide the location for the feed data retrieval."
     )
 
     args = parser.parse_args()
