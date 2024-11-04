acb971a346b0513bced416342c3e7c86456e7e07
Michael Reuter <mareuternh@gmail.com>
2024-11-03 19:56:35 -0700

    Convert to scatter plot for min/max time of day.

--- src/aio_stats/plotting/creators.py
+++ src/aio_stats/plotting/creators.py
@@ -7,7 +7,7 @@
 import pandas as pd
 import plotly.graph_objects as go
 
-__all__ = ["make_min_max_dist", "make_stats_trend"]
+__all__ = ["make_min_max_dist", "make_min_max_scatter", "make_stats_trend"]
 
 
 def make_min_max_dist(type: str, fig: go.Figure, df: pd.DataFrame) -> None:
@@ -39,6 +39,42 @@ def make_min_max_dist(type: str, fig: go.Figure, df: pd.DataFrame) -> None:
     fig.add_trace(min_time_trace)
     fig.add_trace(max_time_trace)
     fig.update_xaxes(title_text="Hour in Day", range=(0, 24))
+    fig.update_layout(title=dict(text=plot_title, xanchor="center", x=0.5))
+
+
+def make_min_max_scatter(type: str, fig: go.Figure, df: pd.DataFrame) -> None:
+    plot_title = "Time in Day of Min/Max "
+    if type == "Temp":
+        plot_title += "Temperature"
+    if type == "RH":
+        plot_title += "Relative Humidity"
+
+    t_min = [
+        (
+            x.to_pydatetime() - x.to_pydatetime().replace(hour=0, minute=0, second=0)
+        ).seconds
+        / 3600
+        for x in df["time_of_min"]
+    ]
+    t_max = [
+        (
+            x.to_pydatetime() - x.to_pydatetime().replace(hour=0, minute=0, second=0)
+        ).seconds
+        / 3600
+        for x in df["time_of_max"]
+    ]
+
+    min_time_trace = go.Scatter(
+        mode="markers", x=df.day, y=t_min, marker_size=15, name="min"
+    )
+    max_time_trace = go.Scatter(
+        mode="markers", x=df.day, y=t_max, marker_size=15, name="max"
+    )
+
+    fig.add_trace(min_time_trace)
+    fig.add_trace(max_time_trace)
+    fig.update_xaxes(title_text="Day in Month", tickmode="array", tickvals=df.day)
+    fig.update_yaxes(title_text="Hour in Day", range=(-0.5, 24.5))
     fig.update_layout(title=dict(text=plot_title, xanchor="center", x=0.5))
 
 
