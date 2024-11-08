145faf89af59c235e3f16abf03b5dc7dcf08f154
Michael Reuter <mareuternh@gmail.com>
2024-11-08 08:52:19 -0700

    Only use AioFile for raw data plotting.

--- src/aio_stats/plotting/plot_raw.py
+++ src/aio_stats/plotting/plot_raw.py
@@ -5,12 +5,14 @@
 import argparse
 from datetime import datetime
 import pathlib
+import tomllib
 from zoneinfo import ZoneInfo
 
 import plotly.graph_objects as go
 import plotly.io as pio
 
 from ..aio_file import AioFile
+from ..helpers import load_feed_settings
 from .raw_data import make_line_plot
 from ..stats_maker import StatsMaker
 
@@ -32,17 +34,16 @@ def main(opts: argparse.Namespace) -> None:
     else:
         end_dt = opts.end
 
-    if "file_path" in opts:
-        af = AioFile(opts.file_path.expanduser())
-        af.read_data()
-        data_records = af.read_data()
-        data = af.transform_data(data_records, opts.timezone)
-        name = opts.file_path.name.split("-")[0].lower()
-        if opts.file_path.name.startswith("Temperature"):
-            short = "Temp"
+    af = AioFile(opts.file_path.expanduser())
+    af.read_data()
+    data_records = af.read_data()
+    data = af.transform_data(data_records, opts.timezone)
+    name = opts.file_path.name.split("-")[0].lower()
+    if "_" in name:
+        name = name.replace("_", "-")
 
-    if "feed" in opts:
-        print("B")
+    shorts = load_feed_settings()["shorts"]
+    short = shorts[name]
 
     stats = StatsMaker()
     stats.create_dataframe(data, name)
@@ -50,39 +51,45 @@ def main(opts: argparse.Namespace) -> None:
 
     fig = go.Figure(layout=layout)
 
-    make_line_plot("Test", short, fig, stats.df)
+    if opts.plot_info:
+        info = tomllib.loads(opts.plot_info.expanduser().read_text())
+        file_stem = info["file_stem"]
+        plot_title = info["plot_title"]
+    else:
+        file_stem = "test"
+        plot_title = file_stem.capitalize()
 
-    fig_file: pathlib.Path = "test.svg"
-    fig.write_image(fig_file)
+    make_line_plot(plot_title, short, fig, stats.df)
+
+    if opts.html:
+        fig.write_html(f"{file_stem}.html")
+    else:
+        fig.write_image(f"{file_stem}.svg")
 
 
 def runner() -> None:
     parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", type=pathlib.Path, help="Path to the data file.")
+    parser.add_argument(
+        "timezone",
+        type=str,
+        help="The timezone for datetime conversion as data is in UTC.",
+    )
     parser.add_argument(
         "--start", help="The datetime for the beginning of the data in ISO-8601 format."
     )
     parser.add_argument(
         "--end", help="The datetime for the end of the data in ISO-8601 format."
     )
+
     parser.add_argument(
-        "--timezone",
-        type=str,
-        required=True,
-        help="The timezone for datetime conversion as data is in UTC.",
+        "--plot-info",
+        type=pathlib.Path,
+        help="Specify a file containing information for the plot.",
     )
 
-    subparsers = parser.add_subparsers(help="subcommand help")
-
-    data_file = subparsers.add_parser("file", help="Read and plot a data file.")
-    data_file.add_argument(
-        "file_path", type=pathlib.Path, help="Path to the data file."
-    )
-
-    client_call = subparsers.add_parser(
-        "client", help="Read data from Adafruit IO and plot."
-    )
-    client_call.add_argument(
-        "feed", help="The Adafruit IO feed name to gather data from."
+    parser.add_argument(
+        "--html", action="store_true", help="Create HTML plot instead of SVG."
     )
 
     args = parser.parse_args()
